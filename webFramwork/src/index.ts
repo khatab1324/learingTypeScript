d11f7be22a92c66688172d721ff72b4655c89011
kahtab1324 <k.s.fayyad.dev@gmail.com>
2024-04-01 23:16:35 +0300

    some edites on pre push

--- webFramwork/src/index.ts
+++ webFramwork/src/index.ts
@@ -1,35 +1,16 @@
-<<<<<<< HEAD
-import { Model } from "./model/Model";
 import { User } from "./model/User";
 
-const user = User.buildUser({ name: "khss", age: 19 });
-user.save();
-user.on("change", () => {
-  console.log("user was change", user);
-}); //this will call the refrence that will execute the function
+const user = User.buildUser({ name: "user", age: 10 });
 user.on("save", () => {
-  console.log("the user are saved");
+  console.log("user are saved");
 });
-
-user.set({ id: 8, name: "sooso", age: 20 });
-user.fetch();
+user.on("change", () => {
+  console.log("user are change");
+});
+user.on("err", () => {
+  console.log("error");
+});
+user.get("id");
 user.save();
-=======
-import { User } from "./model/User";
-import { UsreApi } from "./model/UserApi";
-
-const newUser = new User({ name: "sosom", age: 21 });
-
-// newUser.fetch();
-// setTimeout(() => {
-//   console.log(newUser.get("name"));
-// }, 1000);
-// console.log(newUser.get("name"));
-
-const userApi = new UsreApi("http://localhost:3000");
-userApi.save(newUser);
-(async () => {
-  await userApi.fetchAll();
-  console.log(userApi.userData); 
-})();
->>>>>>> 5c19ece26f52b715c5459284e939009ae0940cc0
+user.set({ name: "jiji", age: 20, city: "amman" });
+user.save();
--- webFramwork/src/model/User.ts
+++ webFramwork/src/model/User.ts
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
+import axios, { AxiosResponse } from "axios";
+import { Model } from "./Model";
+import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
 import { SyncApi } from "./SyncApi";
-import { Attributes } from "./Attributes";
-import { AxiosResponse } from "axios";
-import { Model } from "./Model";
-=======
-import axios, { AxiosResponse } from "axios";
->>>>>>> 5c19ece26f52b715c5459284e939009ae0940cc0
 
 export interface UserProp {
   id?: number;
@@ -15,7 +11,6 @@ export interface UserProp {
   city?: string;
 }
 
-<<<<<<< HEAD
 export class User extends Model<UserProp> {
   static buildUser(attrs: UserProp): User {
     return new User(
@@ -23,36 +18,5 @@ export class User extends Model<UserProp> {
       new Eventing(),
       new SyncApi<UserProp>("http://localhost:3000/users")
     );
-=======
-export class User {
-  constructor(private data: UserProp) {}
-  get(username: string): number | string {
-    return this.data[username];
-  }
-
-  set(updateData: UserProp): void {
-    // this.data = updateData;//search can we do this
-    Object.assign(this.data, updateData);
-  }
-  getCurrentData() {
-    return this.data;
-  }
-
-  async fetch() {
-    await axios
-      .get(`http://localhost:3000/users/${this.get("id")}`)
-      .then((response: AxiosResponse) => {
-        this.set(response.data);
-      });
-  }
-
-  save() {
-    const id = this.get("id");
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post("http://localhost:3000/users/", this.data);
-    }
->>>>>>> 5c19ece26f52b715c5459284e939009ae0940cc0
   }
 }
